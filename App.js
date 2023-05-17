2e9eb1154fe7b54c5d5c8147353c1ba2232d821e
jakeRPowell <ja.cobr@outlook.com>
2023-05-17 11:49:55 +0100

    install drawer navigation

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
