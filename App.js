b4aed7eeb6c0a2386a7263705eb18e6bd70d50a8
jakeRPowell <ja.cobr@outlook.com>
2023-05-18 11:07:03 +0100

    add and configure drawer nav

--- App.js
+++ App.js
@@ -4,10 +4,34 @@ import { StyleSheet } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
+import FavouritesScreen from './screens/FavouritesScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: 'pink',
+        },
+        headerTintColor: 'brown',
+      }}
+    >
+      <Drawer.Screen
+        name="CategoriesChild"
+        component={CategoriesScreen}
+        options={{
+          title: 'Categories',
+        }}
+      />
+      <Drawer.Screen name="Favourites" component={FavouritesScreen} />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   const test = 'hello';
@@ -23,7 +47,13 @@ export default function App() {
             },
           }}
         >
-          <Stack.Screen name="Categories" component={CategoriesScreen} />
+          <Stack.Screen
+            name="Categories"
+            component={DrawerNavigator}
+            options={{
+              headerShown: false,
+            }}
+          />
           <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
           <Stack.Screen name="MealsDetail" component={MealDetailScreen} />
         </Stack.Navigator>
